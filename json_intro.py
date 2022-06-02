fbc99e35f9132a79a99aa132556bfa391f72cb6d
kode2go <brsbin001@myuct.ac.za>
2022-06-02 12:37:58 +0200

    Update json_intro.py

--- json_intro.py
+++ json_intro.py
@@ -1,3 +1,31 @@
 '''
 JSON Intro
 '''
+#!/usr/bin/env python3
+
+# Import json module
+import json
+# https://linuxhint.com/search_json_python/
+# Define json data
+customerData ="""{
+    "id": "3425678",
+    "name": "John Micheal",
+    "email": "john@gmail.com",
+    "type": "regular",
+    "address": "4258 Poplar Chase Lane, Boise, Idaho."
+}"""
+
+# Input the key value that you want to search
+keyVal = "email"
+
+# load the json data
+customer = json.loads(customerData)
+# Search the key value using 'in' operator
+if keyVal in customer:
+    # Print the success message and the value of the key
+    print("%s is found in JSON data" %keyVal)
+    print("The value of", keyVal,"is", customer[keyVal])
+else:
+    # Print the message if the value does not exist
+    print("%s is not found in JSON data" %keyVal)
+    
