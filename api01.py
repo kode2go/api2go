ed30e14f23cb4d4a1775a08c91372aa88e3ad052
kode2go <brsbin001@myuct.ac.za>
2022-06-02 12:28:03 +0200

    Update api01.py

--- api01.py
+++ api01.py
@@ -9,3 +9,23 @@ https://www.tutorialspoint.com/requests/requests_file_upload.htm
 
 
 '''
+
+# Part 1
+
+# Get
+payload = {'id': '9', 'username': 'Delphine'}
+res = requests.get('https://jsonplaceholder.typicode.com/users',params = payload)
+print(res)
+
+# Post
+res = requests.post('https://jsonplaceholder.typicode.com/users', data = {'id':'9', 'username':'Delphine'})
+print(res)
+
+# Put
+res = requests.put('https://jsonplaceholder.typicode.com/users', data =
+{'id':'9', 'username':'Delphine'})
+print(res)
+
+# Delete
+res = requests.delete('https://jsonplaceholder.typicode.com/users')
+print(res)
