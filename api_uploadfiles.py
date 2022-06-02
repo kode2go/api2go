5f368878ab9bfa4b752afe7a2e15a516baecc08c
kode2go <brsbin001@myuct.ac.za>
2022-06-02 12:25:12 +0200

    Create api_uploadfiles.py

---
+++ api_uploadfiles.py
@@ -0,0 +1,7 @@
+'''
+More On Uploading files
+https://www.adamsmith.haus/python/answers/how-to-upload-a-file-with-post-in-python
+https://forpythons.com/how-to-upload-file-with-python-requests/
+https://stackabuse.com/how-to-upload-files-with-pythons-requests-library/
+https://stackoverflow.com/questions/52975930/python-rest-client-api-to-upload-a-file
+'''
